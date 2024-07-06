4e69bf03afb3a6584d3a8ee830d39dbda2b96314
rothfick <robertoo256@icloud.com>
2024-07-06 14:45:13 +0200

    Initial commit

---
+++ src/test/java/org/LearnigPath/pages/GoogleSearchResultsPage.java
@@ -0,0 +1,11 @@
+package org.LearnigPath.pages;
+
+import com.github.jsdevel.testng.selenium.AbstractPage;
+import java.net.URL;
+
+public class GoogleSearchResultsPage extends AbstractPage<GoogleSearchResultsPage, SamplePageFactory> {
+  @Override
+  public boolean isPageViewableFrom(URL proposedUrl) {
+    return "/search".equals(proposedUrl.getPath());
+  }
+}
---
+++ src/test/java/org/LearnigPath/pages/SamplePageFactory.java
@@ -0,0 +1,13 @@
+package org.LearnigPath.pages;
+
+import com.github.jsdevel.testng.selenium.PageFactory;
+
+public interface SamplePageFactory extends PageFactory {
+  GoogleHomePage getHomePage();  
+  GoogleHomePage getHomePage(String path);  
+  GoogleSearchResultsPage getSearchResultsPage();
+  GoogleSearchResultsPage getSearchResultsPage(String path);
+
+  // Used to throw an Exception in the test.
+  String getFoo();
+}
